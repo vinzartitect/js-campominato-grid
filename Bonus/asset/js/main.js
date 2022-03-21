329d60a859947459ded25ea816d3574a364b1ff3
VincenzoBorriello89 <vincborr89@gmail.com>
2022-03-21 18:58:11 +0100

    esercizio bonus fatto

---
+++ Bonus/asset/js/main.js
@@ -0,0 +1,102 @@
+// Griglia Campo Minato
+// Generare una griglia di gioco quadrata, simile a quella dello screenshot, 
+// in cui ogni cella contiene un numero tra 1 e 100.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+
+// seleziono l'id grid dall html e gli do una variabile let
+let grid = document.getElementById('grid');
+
+let select = document.getElementById('scelte');
+
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click', function() {
+
+    grid.innerHTML = '';
+
+    if (select.value === `diff_uno`) {
+
+        // creo ciclo for per svilupare i box interni alla griglia
+        for (let i = 1; i <= 100; i++) {
+
+            // creo una variabile box inserendoci dentro un elemento div creato
+            let box = document.createElement('div');
+
+            // aggiungo il testo dei numeri in senso continuo, non random
+            box.innerHTML += i;
+
+            // aggiungo una classe al div con dentro box
+            box.classList.add('box');
+
+            // incolliamo let box e il suo contenuto all'interno del div con id=grid in html
+            grid.appendChild(box);
+
+            // funzione al click
+            box.addEventListener('click', function () {
+                console.log(this);
+                this.classList.add('clicked');
+                console.log(this.innerHTML);
+                let numero = this.innerHTML;
+            });
+
+        }
+
+    } else if (select.value === `diff_due`) {
+
+        // creo ciclo for per svilupare i box interni alla griglia
+        for (let i = 1; i <= 80; i++) {
+
+            // creo una variabile box inserendoci dentro un elemento div creato
+            let box = document.createElement('div');
+
+            // aggiungo il testo dei numeri in senso continuo, non random
+            box.innerHTML += i;
+
+            // aggiungo una classe al div con dentro box
+            box.classList.add('box');
+
+            // incolliamo let box e il suo contenuto all'interno del div con id=grid in html
+            grid.appendChild(box);
+
+            // funzione al click
+            box.addEventListener('click', function () {
+                console.log(this);
+                this.classList.add('clicked');
+                console.log(this.innerHTML);
+                let numero = this.innerHTML;
+            });
+
+        }
+
+    } else if (select.value === `diff_tre`) {
+
+        // creo ciclo for per svilupare i box interni alla griglia
+        for (let i = 1; i <= 50; i++) {
+
+            // creo una variabile box inserendoci dentro un elemento div creato
+            let box = document.createElement('div');
+
+            // aggiungo il testo dei numeri in senso continuo, non random
+            box.innerHTML += i;
+
+            // aggiungo una classe al div con dentro box
+            box.classList.add('box');
+
+            // incolliamo let box e il suo contenuto all'interno del div con id=grid in html
+            grid.appendChild(box);
+
+            // funzione al click
+            box.addEventListener('click', function () {
+                console.log(this);
+                this.classList.add('clicked');
+                console.log(this.innerHTML);
+                let numero = this.innerHTML;
+            });
+
+        }
+
+    }
+
+    }
+
+)
