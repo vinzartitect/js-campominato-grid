67887ab802ff30b61e185a28dd53bf6f00a048f0
VincenzoBorriello89 <vincborr89@gmail.com>
2022-03-21 16:29:36 +0100

    esercizio base completato

---
+++ Consegna base/asset/js/main.js
@@ -0,0 +1,36 @@
+// Griglia Campo Minato
+// Generare una griglia di gioco quadrata, simile a quella dello screenshot, 
+// in cui ogni cella contiene un numero tra 1 e 100.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+
+// seleziono l'id grid dall html e gli do una variabile let
+let grid = document.getElementById('grid');
+
+// creo ciclo for per svilupare i box interni alla griglia
+for ( let i = 1; i <= 100; i++) {
+
+    // creo una variabile box inserendoci dentro un elemento div creato
+    let box = document.createElement('div');
+    
+    // aggiungo il testo dei numeri in senso continuo, non random
+    box.innerHTML += `<div>${i}</div>`;
+
+    // aggiungo una classe al div con dentro box
+    box.classList.add('box');
+
+    // incolliamo let box e il suo contenuto all'interno del div con id=grid in html
+    grid.appendChild(box);
+
+    // funzione al click
+    box.addEventListener('click', function(){
+        console.log(this);
+        this.classList.add('clicked');
+        console.log(this.innerHTML);
+        let numero = this.innerHTML;
+    });
+}
+
+
+
+
+
